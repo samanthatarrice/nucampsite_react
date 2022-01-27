22b9c6747bdb33bf86c45b0eb6292649c5c10484
Samantha Tarrice <s.tarrice@gmail.com>
2022-01-27 16:32:11 -0300

    First commit with react site

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+function RenderCampsite({campsite}) {
+  return (
+    <div className="col-md-5 m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+function RenderComments({comments}) {
+  if (comments) {
+    return (
+      <div className="col-md-5 m-1">
+        <h4 className="mb-3">Comments</h4>
+        <div>
+        {comments.map(comment => {
+          return (
+            <figure key={comment.id}>
+              <blockquote className="mb-0">{comment.text}</blockquote>
+              <figcaption>-- {comment.author}, <span>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</span></figcaption>
+            </figure>
+          )
+        })}
+        </div>
+      </div>
+    );
+  }
+  return <div />
+}
+  
+function CampsiteInfo(props) {
+  if (props.campsite) {
+    return (
+      <div className="container">
+        <div className="row">
+            <RenderCampsite campsite={props.campsite} />
+            <RenderComments comments={props.campsite.comments} />
+        </div>
+      </div>
+    );
+  }
+  return <div />;
+}
+
+
+export default CampsiteInfo;
