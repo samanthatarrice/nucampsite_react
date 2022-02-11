9da16b0037b9baab308118d4cf0621423e20d586
Samantha Tarrice <s.tarrice@gmail.com>
2022-02-11 13:13:56 -0300

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -48,7 +48,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
@@ -98,7 +98,7 @@ class CommentForm extends Component {
   }
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
   if (comments.length) {
     return (
       <div className="col-md-5 m-1">
@@ -113,7 +113,7 @@ function RenderComments({comments, addComment, campsiteId}) {
           )
         })}
         </div>
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -158,7 +158,7 @@ function CampsiteInfo(props) {
             <RenderCampsite campsite={props.campsite} />
             <RenderComments 
               comments={props.comments} 
-              addComment={props.addComment}
+              postComment={props.postComment}
               campsiteId={props.campsite.id}
             />
         </div>
