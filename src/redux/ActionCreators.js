d7859687810b548d225147a7feb6e199457ad109
Samantha Tarrice <s.tarrice@gmail.com>
2022-02-16 12:44:37 -0300

    Final commit for React course

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -81,6 +81,7 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
         author: author,
         text: text
     };
+
     newComment.date = new Date().toISOString();
 
     return fetch(baseUrl + 'comments', {
@@ -108,6 +109,36 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
             alert('Your comment could not be posted\nError: ' + error.message);
         });
 };
+
+export const postFeedback = feedback => () => {
+
+    return fetch(baseUrl + 'feedback', {
+            method: "POST",
+            body: JSON.stringify(feedback),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(response => {
+            alert('Thank you for your feedback!\n' + JSON.stringify(response))
+        })
+        .catch(error => {
+            console.log('post feedback', error.message);
+            alert('Your feedback could not be posted\nError: ' + error.message);
+        });
+}
 
 export const fetchPromotions = () => dispatch => {
     dispatch(promotionsLoading());
@@ -140,7 +171,48 @@ export const promotionsFailed = errMess => ({
     payload: errMess
 });
 
+
+
 export const addPromotions = promotions => ({
     type: ActionTypes.ADD_PROMOTIONS,
     payload: promotions
 });
+
+export const fetchPartners = () => dispatch => {
+    dispatch(partnersLoading());
+    return fetch(baseUrl + 'partners')
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    console.log(`This is the error:`, error);
+                    console.log(`This is the response:`, response);
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(partners => dispatch(addPartners(partners)))
+        .catch(error => dispatch(partnersFailed(error.message)));
+};
+
+export const partnersLoading = () => ({
+    type: ActionTypes.PARTNERS_LOADING
+});
+
+export const partnersFailed = errMess => ({
+    type: ActionTypes.PARTNERS_FAILED,
+    payload: errMess
+});
+
+export const addPartners = partners => ({
+    type: ActionTypes.ADD_PARTNERS,
+    payload: partners
+});
+
